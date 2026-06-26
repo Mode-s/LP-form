305f0e5cd90e6c946d8f5f34b00ebe70124170d9
Mode-s <shota.umemoto7@gmail.com>
2026-06-26 16:44:14 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
-import Link from 'next/link';
 import styles from "./page.module.css";
+import { signIn } from "@/auth";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.inner}>
-        <h1 className={styles.title}>LP制作依頼フォーム</h1>
+        <h1 className={styles.title}>LP制作依頼管理</h1>
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google", { redirectTo: "/dashboard" });
+          }}
+        >
+          <button type="submit" className={styles.loginButton}>
+            Googleでログイン
+          </button>
+        </form>
       </div>
-      <Link href="/request" className={styles.loginButton}>Googleでログイン</Link>
     </main>
   );
 }
